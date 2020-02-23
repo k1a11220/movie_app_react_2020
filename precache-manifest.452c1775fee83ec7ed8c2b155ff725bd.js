b1d7ff2aa3a3ec9ca788022c6671ee1ed4e7b24b
Son Beomsoo <k1a11220@naver.com>
2020-02-23 17:28:29 +0900

    Updates

---
+++ precache-manifest.452c1775fee83ec7ed8c2b155ff725bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b48accca37462ac778a9456453fdd7",
+    "url": "/react_movie-app/index.html"
+  },
+  {
+    "revision": "6a81c4790e5b21f7b29f",
+    "url": "/react_movie-app/static/css/main.68e2dd14.chunk.css"
+  },
+  {
+    "revision": "61304aac5fec6623c05b",
+    "url": "/react_movie-app/static/js/2.b9191a52.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react_movie-app/static/js/2.b9191a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a81c4790e5b21f7b29f",
+    "url": "/react_movie-app/static/js/main.49a02702.chunk.js"
+  },
+  {
+    "revision": "29eb72728a906a92e541",
+    "url": "/react_movie-app/static/js/runtime-main.4c4ad0ac.js"
+  }
+]);

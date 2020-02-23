1be44d5e214b121903cb32dff3e4f15542f69416
Son Beomsoo <k1a11220@naver.com>
2020-02-23 16:49:25 +0900

    Add Router

---
+++ build/precache-manifest.00d96210ec941a323c2b449a4b0adb09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626c3e1a5321bd716f636f2dc453585c",
+    "url": "/movie_app_react_2020/index.html"
+  },
+  {
+    "revision": "6a81c4790e5b21f7b29f",
+    "url": "/movie_app_react_2020/static/css/main.68e2dd14.chunk.css"
+  },
+  {
+    "revision": "61304aac5fec6623c05b",
+    "url": "/movie_app_react_2020/static/js/2.b9191a52.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie_app_react_2020/static/js/2.b9191a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a81c4790e5b21f7b29f",
+    "url": "/movie_app_react_2020/static/js/main.49a02702.chunk.js"
+  },
+  {
+    "revision": "5e7c4e1ba11f7684df63",
+    "url": "/movie_app_react_2020/static/js/runtime-main.076dc94a.js"
+  }
+]);
---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return <span>ddfdss</span>;
+}
+
+export default About;
